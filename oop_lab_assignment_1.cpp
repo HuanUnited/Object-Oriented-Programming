27055b97b836a33bb57c8f3f031c79c9d639193b
Duong Cong Huan <huanthelegend@gmail.com>
2025-09-10 19:41:20 +0700

    removed other poles in ooplab1

--- oop_lab_assignment_1.cpp
+++ oop_lab_assignment_1.cpp
@@ -4,13 +4,10 @@ using std::cin, std::cout, std::endl, std::sqrt, std::atan;
 
 class Triangle {
 private:
-    int m_coord_x;
-    int m_coord_y;
+    double m_coord_x;
+    double m_coord_y;
     double m_height;
     double m_base;
-    double m_side;
-    double m_base_angle;   // angle at base
-    double m_small_angle;  // top angle
 
 public:
     // Triangle I/O
@@ -19,8 +16,6 @@ public:
         m_coord_y = y;
         m_height = height;
         m_base = base;
-        updateSide();
-        updateAngles();
     }
 
     void inputTriangle() { // Hand input
@@ -28,26 +23,13 @@ public:
         cout << "Input y: "; cin >> m_coord_y;
         cout << "Input height: "; cin >> m_height;
         cout << "Input base: "; cin >> m_base;
-        updateSide();
-        updateAngles();
     }
 
     void outputTriangle() const {
         cout << "Triangle:\n";
         cout << "A: (" << m_coord_x << "," << m_coord_y << ")\n";
         cout << "Base: " << m_base << " Height: " << m_height << "\n";
-        cout << "Side: " << m_side << " Perimeter: " << getPerimeter() << "\n";
-    }
-
-    // Update
-    void updateSide() {
-        m_side = sqrt((m_base / 2) * (m_base / 2) + m_height * m_height);
-    }
-
-    void updateAngles() {
-        // angles in degrees
-        m_base_angle = atan((m_base / 2) / m_height) * 180.0 / M_PI;
-        m_small_angle = 180.0 - 2 * m_base_angle;
+        cout << "Side: " << getSide() << " Perimeter: " << getPerimeter() << "\n";
     }
 
     // Operators
@@ -55,41 +37,36 @@ public:
         Triangle scaled = *this;
         scaled.m_base *= k;
         scaled.m_height *= k;
-        scaled.updateSide();
-        scaled.updateAngles();
         return scaled;
     }
     /*This pointer is a pointer that points to the current instance of the class.*/
 
     bool operator==(const Triangle &other) const {
-        return (fabs(m_base - other.m_base) < 1e-6 &&
-                fabs(m_height - other.m_height) < 1e-6 &&
-                m_coord_x == other.m_coord_x &&
-                m_coord_y == other.m_coord_y);
+        return ((m_base == other.m_base) && (m_height == other.m_height)) ? true : false;
     }
 
     // Getter&Setters
-    void setX(int x) { m_coord_x = x; }
-    void setY(int y) { m_coord_y = y; }
-    void setHeight(double h) { m_height = h; updateSide(); updateAngles(); }
-    void setBase(double b) { m_base = b; updateSide(); updateAngles(); }
+    void setX(double x) { m_coord_x = x; }
+    void setY(double y) { m_coord_y = y; }
+    void setHeight(double h) { m_height = h; }
+    void setBase(double b) { m_base = b; }
 
-    int getX() const { return m_coord_x; }
-    int getY() const { return m_coord_y; }
+    double getX() const { return m_coord_x; }
+    double getY() const { return m_coord_y; }
     double getHeight() const { return m_height; }
     double getBase() const { return m_base; }
-    double getSide() const { return m_side; }
-    double getSmallAngle() const { return m_small_angle; }
-    double getBaseAngle() const { return m_base_angle; }
+    double getSide() const { return sqrt((m_base / 2) * (m_base / 2) + m_height * m_height); }
+    double getSmallAngle() const { return 180.0 - 2 * getBaseAngle(); }
+    double getBaseAngle() const { return atan((m_base / 2) / m_height) * 180.0 / M_PI; }
 
     // GeoGebra
-    double getPerimeter() const { return 2 * m_side + m_base; }
+    double getPerimeter() const { return 2 * getSide() + m_base; }
     double getArea() const { return 0.5 * m_base * m_height; }
-    double getSemiPerimeter() const { return (m_base + 2 * m_side) / 2.0; }
-    double getSideHeight() const { return (2 * getArea() / m_side); }
+    double getSemiPerimeter() const { return (m_base + 2 * getSide()) / 2.0; }
+    double getSideHeight() const { return (2 * getArea() / getSide()); }
     double getCircumradius() const {
         double S = getArea();
-        return (m_base * m_side * m_side) / (4 * S);
+        return (m_base * getSide() * getSide()) / (4 * S);
     }
     void getCircumcenter() const { // Coordinates
         double R = getCircumradius();
@@ -100,7 +77,7 @@ public:
 
     // Checker
     bool isEquilateral() const {
-        return m_base == m_side? true : false;
+        return m_base == getSide()? true : false;
     }
     /*fabs is a function that returns the absolute value*/
 
@@ -110,8 +87,6 @@ public:
         m_coord_y = 0;
         m_height = 1;
         m_base = 1;
-        updateSide();
-        updateAngles();
     }
 
     Triangle(int x, int y, double height, double base) {
@@ -119,8 +94,6 @@ public:
         m_coord_y = y;
         m_height = height;
         m_base = base;
-        updateSide();
-        updateAngles();
     }
 };
 
